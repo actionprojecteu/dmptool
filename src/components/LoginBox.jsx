be97f3183921df859a20be5f545c980a8eab49c2
Esteban Gonzalez Guardia <egonzalez@ciclope.info>
2020-02-16 21:50:10 +0100

    Authetication login added

--- src/components/LoginBox.jsx
+++ src/components/LoginBox.jsx
@@ -5,24 +5,21 @@ class LoginBox extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {username:'',password:'',access_token:''};
-    this.handleTextArea = this.handleTextArea.bind(this);
+
+    this.state = {
+      username: '',
+      password: '',
+      submitted: false,
+      loading: false,
+      error: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
   }
 
-  handleTextArea(e) {
-    let value = e.target.value;
-    let name = e.target.name;
 
-    this.setState(prevState => ({
-
-        ...prevState, [name]: value
-
-      }), ()=>console.log(this.state))
-
-
-  }
-  //https://api.dmptool.actionproject.eu/login
 
   test(e) {
     console.log("Test");
@@ -39,70 +36,95 @@ class LoginBox extends React.Component {
     })
   }
 
-  submitLogin(e) {
-    var auth = "Basic "+btoa(this.state.username+":"+this.state.password);
-    fetch('https://api.dmptool.actionproject.eu/login',{
-				method: "GET",
-        mode: 'cors',
-        headers : {
-    			"Authorization" : auth
-    		},
-			}).then(response => {
-				response.json().then(data =>{
-					console.log("Login:" + JSON.stringify(data));
-          console.log("Status:"+response.status);
-          if (response.status == 200){
-            console.log("Log in the system");
-            //this.setState({access_token:data.access_token});
-            this.setState(data);
-            localStorage.setItem("token",data.access_token);
-            localStorage.setItem("username",data.username);
-          }
-					//this.setState({dmps:JSON.parse(JSON.stringify(data))})
-					//this.setState({dmps:data});
-				})
-		})
-  }
+  handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
 
-  render() {
-    return (
-      <div className="inner-container">
-        <div className="header">
-          Login
-        </div>
-        <div className="box">
+    handleSubmit(e) {
+        e.preventDefault();
 
-          <Input inputType={'text'}
-              name={'username'}
-              title= {'Username'}
-              value={this.state.username}
-              handleChange={this.handleTextArea}
-          />
+        this.setState({ submitted: true });
+        const { username, password, returnUrl } = this.state;
 
-        <Input inputType={'password'}
-              name={'password'}
-              title= {'Password'}
-              value={this.state.password}
-              handleChange={this.handleTextArea}
-          />
+        // stop here if form is invalid
+        if (!(username && password)) {
+            return;
+        }
 
-          <button
-            type="button"
-            className="login-btn"
-            onClick={this
-            .submitLogin
-            .bind(this)}>Login</button>
+        this.setState({ loading: true });
 
-            <button
-              type="button"
-              className="login-btn"
-              onClick={this
-              .test
-              .bind(this)}>Test</button>
-        </div>
-      </div>
-    );
-  }
+        var auth = "Basic "+btoa(username+":"+password);
 
+            fetch('https://api.dmptool.actionproject.eu/login',{
+        				method: "GET",
+                mode: 'cors',
+                headers : {
+            			"Authorization" : auth
+            		},
+        			}).then(response => {
+        				response.json().then(data =>{
+        					console.log("Login:" + JSON.stringify(data));
+                  console.log("Status:"+response.status);
+                  if (response.status == 200){
+                    console.log("Log in the system");
+                    //this.setState({access_token:data.access_token});
+                    //this.setState(data);
+                    localStorage.setItem("token",data.access_token);
+                    localStorage.setItem("username",data.username);
+                    localStorage.setItem("project",data.project);
+                    localStorage.setItem("project_description",data.project_description);
+
+                    const { from } = this.props.location.state || { from: { pathname: "/" } };
+                    this.props.history.push(from);
+                  }
+        					//this.setState({dmps:JSON.parse(JSON.stringify(data))})
+        					//this.setState({dmps:data});
+        				})
+        		})
+/*
+        userService.login(username, password)
+            .then(
+                user => {
+                    const { from } = this.props.location.state || { from: { pathname: "/" } };
+                    this.props.history.push(from);
+                },
+                error => this.setState({ error, loading: false })
+            );*/
+    }
+
+    render() {
+        const { username, password, submitted, loading, error } = this.state;
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h2>Login</h2>
+                <form name="form" onSubmit={this.handleSubmit}>
+                    <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
+                        {submitted && !username &&
+                            <div className="help-block">Username is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
+                        {submitted && !password &&
+                            <div className="help-block">Password is required</div>
+                        }
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary" disabled={loading}>Login</button>
+                        {loading &&
+                            <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                        }
+                    </div>
+                    {error &&
+                        <div className={'alert alert-danger'}>{error}</div>
+                    }
+                </form>
+            </div>
+        );
+    }
 }
 export default LoginBox;
