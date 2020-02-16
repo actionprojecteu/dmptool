67090644ef83f093c2d640a819537e93b500bb53
Esteban Gonzalez Guardia <egonzalez@ciclope.info>
2020-02-16 21:49:24 +0100

    Task pooling added

--- src/components/DmpRow.jsx
+++ src/components/DmpRow.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFilePdf, faFileWord, faPencilAlt } from '@fortawesome/free-solid-svg-icons'
+import { faFilePdf, faFileWord, faPencilAlt, faTools, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import DmpFormContainer from '../containers/DmpFormContainer';
 import Modal from "react-bootstrap/Modal";
 
@@ -13,17 +13,54 @@ class DmpRow extends React.Component {
     this.state = {
       hover: false,
       showForm: false,
+      showMessage: false,
+      message: '',
+      showButtons: false,
+      docx_file: '',
+      pdf_file: '',
       dmp: props.dmp
     }
 
     this.handleClick = this.handleClick.bind(this);
 
     this.handleEdit = this.handleEdit.bind(this);
-    this.handleGenerateDoc = this.handleGenerateDoc.bind(this);
-    this.handleGeneratePdf = this.handleGeneratePdf.bind(this);
+    this.handleGenerateFiles = this.handleGenerateFiles.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
 
     this.toggleHover = this.toggleHover.bind(this);
     this.handleClose = this.handleClose.bind(this);
+
+    //this.timer = setInterval(()=> this.getLastTask(this.state.dmp['_id']), 1000);
+    this.timer = setTimeout(()=> this.getLastTask(this.state.dmp['_id']), 1000);
+
+  }
+
+  getLastTask(dmp_id){
+
+    console.log("Loading status last task");
+
+    const token = localStorage.getItem("token");
+
+    fetch('https://api.dmptool.actionproject.eu/tasks?id_dmp='+dmp_id,{
+				method: "GET",
+				headers: {
+          'Authorization': 'Bearer '+token
+				},
+			}).then(response => {
+				response.json().then(data =>{
+          if (data.length > 0){
+    					console.log("Task selected" + JSON.stringify(data[0]) + " with status " + data[0].status);
+    					//this.setState({dmps:JSON.parse(JSON.stringify(data))})
+    					//this.setState({showForm:true, dmp:data});
+              if (data[0].status=="done"){
+                this.setState({"docx_file":"https://api.dmptool.actionproject.eu/resources/docx/"+data[0].url_docx,"pdf_file":"https://api.dmptool.actionproject.eu/resources/pdf/"+data[0].url_pdf});
+                clearInterval(this.timer);
+              }
+          } else {
+            clearInterval(this.timer);
+          }
+				})
+		})
 
   }
 
@@ -45,7 +82,8 @@ class DmpRow extends React.Component {
 				response.json().then(data =>{
 					console.log("Load Data Edit" + JSON.stringify(data));
 					//this.setState({dmps:JSON.parse(JSON.stringify(data))})
-					this.setState({showForm:true, dmp:data})
+					this.setState({showForm:true, dmp:data});
+
 				})
 		})
   }
@@ -54,17 +92,82 @@ class DmpRow extends React.Component {
     this.setState({showForm:false})
   }
 
-  handleGenerateDoc(e){
+  getTask(id,timer){
+    console.log("Get Task:"+id);
+    //this.setState({showForm:true})
+    //this.setState({showForm:true, dmp:{description:'ttt'}})
+    const token = localStorage.getItem("token");
+
+    fetch('https://api.dmptool.actionproject.eu/tasks/'+id,{
+				method: "GET",
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json',
+          'Authorization': 'Bearer '+token
+				},
+			}).then(response => {
+				response.json().then(data =>{
+					console.log("Get Task" + JSON.stringify(data));
+
+					//this.setState({dmps:JSON.parse(JSON.stringify(data))})
+					//this.setState({showForm:true, dmp:data});
+
+				})
+		}).catch(function() {
+        console.log("error");
+        console.log(this);
+        //setTimeout(()=> this.getTask(id), 4000);
+        clearInterval(timer);
+        //this.state.timer=null;
+    });
 
   }
 
-  handleGeneratePdf(e){
+  handleGenerateFiles(e){
+    const token = localStorage.getItem("token");
+    const data = {"dmp":this.state.dmp['_id']};
 
+    fetch('https://api.dmptool.actionproject.eu/tasks',{
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer '+token
+        },
+      }).then(response => {
+        response.json().then(data =>{
+          console.log("Successful" + data);
+          console.log(this);
+          this.setState({"showMessage":true,"message":"Generating files ...","task":data.id});
+          this.timer = setInterval(()=> this.getTask(data.id,this.timer), 1000);
+          //setTimeout(()=> this.getTask(data.id), 4000);
+        })
+    })
+  }
+
+  handleDelete(e){
+    e.preventDefault();
+    console.log("Edit DMP");
+    const token = localStorage.getItem("token");
+
+    fetch('https://api.dmptool.actionproject.eu/dmps/'+this.state.dmp._id,{
+				method: "DELETE",
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json',
+          'Authorization': 'Bearer '+token
+				},
+			}).then(response => {
+				response.json().then(data =>{
+					console.log("DMP deleted" + JSON.stringify(data));
+				})
+		})
   }
 
   handleClick(e){
     e.preventDefault();
-    console.log('The link was clicked.'+e.name);
+    console.log('The link was clicked.'+e);
   }
 
   toggleHover() {
@@ -73,7 +176,11 @@ class DmpRow extends React.Component {
 
   render() {
 
-    let description = this.props.dmp.description;
+    let description = this.props.dmp.purpose;
+
+    if (description.length > 50){
+      description = description.substring(1,50) + '...';
+    }
 
     if (description == undefined){
         description = 'No description';
@@ -102,11 +209,17 @@ class DmpRow extends React.Component {
             <FontAwesomeIcon style={iconStyle} icon={faPencilAlt} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover} onClick={this.handleEdit} />
           </span>
           <span className="fa-layers fa-fw">
-            <FontAwesomeIcon style={iconStyle} icon={faFilePdf} onClick={this.handleGeneratePdf} onMouseOver={this.handleMouse} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}/>
+            <FontAwesomeIcon style={iconStyle} icon={faTrashAlt} onClick={this.handleDelete} onMouseOver={this.handleMouse} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}/>
           </span>
-          <span className="fa-layers fa-fw">
-            <FontAwesomeIcon style={iconStyle} icon={faFileWord} onClick={this.handleGenerateDoc} onMouseOver={this.onHover} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}/>
+          <span className="fa-layers fa-fw" style={(this.state.pdf_file)? {iconStyle} : { iconStyle, display: 'none' }}>
+            <a href={this.state.pdf_file} download><FontAwesomeIcon  icon={faFilePdf} onMouseOver={this.onHover} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}/></a>
           </span>
+          <span className="fa-layers fa-fw" style={(this.state.docx_file)? {iconStyle} : { iconStyle, display: 'none' }}>
+            <a href={this.state.docx_file} download><FontAwesomeIcon icon={faFileWord} onMouseOver={this.onHover} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}/></a>
+          </span>
+        </div>
+        <div><span style={(this.state.showMessage)? {} : { display: 'none' }}>{this.state.message}</span></div>
+        <div>
             <Modal show={this.state.showForm} onHide={this.handleClose} animation={false}>
               <Modal.Header closeButton>
                 <Modal.Title>{name}</Modal.Title>
