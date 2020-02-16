765773c22505c0fb02e98e41c253773b10236b6e
Esteban Gonzalez Guardia <egonzalez@ciclope.info>
2020-02-16 21:53:12 +0100

    Logout button added

--- src/header.js
+++ src/header.js
@@ -2,21 +2,38 @@ import React from "react";
 //import { NavLink } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 
+class Header extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          redirect: false
+      }
 
-function Header() {
-  return (
+  }
+  logout = () => {
+      localStorage.clear();
+      this.setState({ redirect: true });
+  }
 
-    <Navbar class="header">
-      <Navbar.Brand href="#home">
-        <img
-          alt="logo"
-          src="https://actionproject.eu/wp-content/uploads/2019/05/actiON.png"
-          height="50px"
-          className="d-inline-block align-top"
-        />{' '}
-        <span style={{textAlign:"center", fontSize:"2em"}}> DMPT - Data Management Plan Tool </span>
-      </Navbar.Brand>
-    </Navbar>
-  );
+  render() {
+
+    return (
+        <Navbar class="header">
+          <Navbar.Brand href="#home">
+            <img
+              alt="logo"
+              src="https://actionproject.eu/wp-content/uploads/2019/05/actiON.png"
+              height="50px"
+              className="d-inline-block align-top"
+            />{' '}
+
+          </Navbar.Brand>
+          <span style={{textAlign:"center", fontSize:"2em"}}> DMPT - Data Management Plan Tool </span>
+          <button className="btn btn-primary" onClick={this.logout}>Logout</button>
+        </Navbar>
+    )
+  }
+
 }
+
 export default Header;
