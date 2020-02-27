63d008420af12c82028cbb9b17ef276497dfcae1
Esteban Gonzalez Guardia <egonzalez@ciclope.info>
2020-02-27 10:58:07 +0100

    Button to link Coney added

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -2,24 +2,36 @@ import React from 'react';
 
 
 const Select = (props) => {
+	//const className = kind === "primary" ? "PrimaryButton" : "SecondaryButton";
+	//const help_button = props.help_assistant === "yes" ?  <button class="btn btn-primary">Help</button>;
+	//const help_button = (props.help_assitant== "yes") ? <button class="btn btn-primary">Help</button>;
+	var helpButton;
+	if (props.help_assistant== "yes") {
+		console.log("Paso1");
+	  helpButton = <button class="btn btn-primary" onClick= {props.action}>Help</button>;
+	}
+
 	return(<div className="form-group">
 			<label for={props.name}> {props.title} </label>
-		    <select
-		      id = {props.name}
-		      name={props.name}
-		      value={props.value}
-		      onChange={props.handleChange}
-		      className="form-control" >
-		      <option value="" disabled >{props.placeholder}</option>
-		      {props.options.map(option => {
-		        return (
-		          <option
-		            key={option}
-		            value={option}
-		            label={option}>{option}</option>
-		        );
-		      })}
-		    </select>
+				<div>
+			    <select
+			      id = {props.name}
+			      name={props.name}
+			      value={props.value}
+			      onChange={props.handleChange}
+			      className="form-control" style={{display:'inline-block',width:'80%',marginRight:'1em'}}>
+			      <option value="" disabled >{props.placeholder}</option>
+			      {props.options.map(option => {
+			        return (
+			          <option
+			            key={option}
+			            value={option}
+			            label={option}>{option}</option>
+			        );
+			      })}
+			    </select>
+					{helpButton}
+				</div>
   </div>)
 }
 
